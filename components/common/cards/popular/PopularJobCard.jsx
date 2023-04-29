b114c536eb027bff44bf6a2e401f18dfa201d3da
Olusola Afikode <afikodeolaitan@gmail.com>
2023-04-29 07:34:21 +0100

    implement popular job cards

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,11 +1,6 @@
 import React from "react";
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  Image
-} from "react-native";
-
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { checkImageURL } from "../../../../utils";
 import styles from "./popularjobcard.style";
 
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
@@ -16,7 +11,16 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
     >
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
         <Image
-          source={{ uri: item.employer_logo }}
+          // source={{
+          //   uri: item.employer_logo
+          //     ? item.employer_logo
+          //     : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          // }}
+          source={{
+            uri: checkImageURL(item.employer_logo)
+              ? item.employer_logo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
           resizeMode="contain"
           style={styles.logoImage}
         />
@@ -26,7 +30,10 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
       </Text>
 
       <View style={styles.infoContainer}>
-        <Text style={styles.jobName(selectedJob, item)}>{item?.job_title}</Text>
+        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+          {item?.job_title}
+        </Text>
+        <Text style={styles.location}>{item?.job_country}</Text>
       </View>
     </TouchableOpacity>
   );
